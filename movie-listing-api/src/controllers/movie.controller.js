621584116aa951765fb167d19572d913e72d554b
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-03-13 15:50:44 +0530

    feat: search filter and pagination

--- movie-listing-api/src/controllers/movie.controller.js
+++ movie-listing-api/src/controllers/movie.controller.js
@@ -4,11 +4,11 @@
 const Movie = require("../models/movie.model");
 
 // Create movie
-/** 
-* - POST /api/movies
-*/
+/**
+ * - POST /api/movies
+ */
 exports.createMovie = async (req, res, next) => {
-  const { title, description, genre, releaseYear } = req.body;
+  const { title, description, genre, releaseYear, averageRating } = req.body;
   try {
     const movie = await Movie.create({
       title,
@@ -16,6 +16,7 @@ exports.createMovie = async (req, res, next) => {
       genre,
       releaseYear,
       createdBy: req.user._id,
+      averageRating,
     });
 
     res.status(201).json({
@@ -28,15 +29,44 @@ exports.createMovie = async (req, res, next) => {
 };
 
 // Get movies
-/** 
-* - GET /api/movies/
-*/
+/**
+ * - GET /api/movies/
+ */
 exports.getMovies = async (req, res, next) => {
   try {
-    const movies = await Movie.find().populate("createdBy", "name email");
+    //1.read query parameter
+    const { search, genre, page = 1, limit = 10 } = req.query;
+    // 2. create filter object
+    const filter = {};
+    // 3.Search by title
+    if (search) {
+      filter.title = {
+        $regex: search,
+        $options: "i",
+      };
+    }
+
+    //4. filter by genre
+    if (genre) {
+      filter.genre = genre;
+    }
+
+    // 5.pagination calculation
+    const skip = (page - 1) * limit;
+
+    const movies = await Movie.find(filter)
+      .skip(skip)
+      .limit(Number(limit))
+      .populate("createdBy", "name email");
+
+    // 6.count total movies
+
+    const totalMovies = await Movie.countDocuments(filter);
 
     res.json({
-      count: movies.length,
+      totalMovies,
+      currentPage: Number(page),
+      totalPage: Math.ceil(totalMovies / limit),
       movies,
     });
   } catch (err) {
@@ -45,9 +75,9 @@ exports.getMovies = async (req, res, next) => {
 };
 
 // get single movie
-/** 
-* - GET /api/movies/:id
-*/
+/**
+ * - GET /api/movies/:id
+ */
 exports.getMovieById = async (req, res, next) => {
   try {
     const movie = await Movie.findById(req.params.id).populate(
@@ -67,9 +97,9 @@ exports.getMovieById = async (req, res, next) => {
 };
 
 // Update movie
-/** 
-* - PUT /api/movies/:id
-*/
+/**
+ * - PUT /api/movies/:id
+ */
 exports.updateMovie = async (req, res, next) => {
   try {
     const movie = await Movie.findByIdAndUpdate()(req.params.id, req.body, {
@@ -93,9 +123,9 @@ exports.updateMovie = async (req, res, next) => {
 };
 
 // Delete movie
-/** 
-* - DELETE /api/movies/:id
-*/
+/**
+ * - DELETE /api/movies/:id
+ */
 exports.deleteMovie = async (req, res, next) => {
   try {
     const movie = await Movie.findByIdAndDelete(req.params.id);
