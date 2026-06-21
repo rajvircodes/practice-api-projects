c092aa3dbc461b3ef363ebc47c56027e4c2c6a8c
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-06-21 11:08:11 +0530

    note created

--- notes-application/backend/src/app.js
+++ notes-application/backend/src/app.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import notesRoutes from './routes/notes.routes.js'
 import cors from 'cors'
+import morgan from 'morgan'
 const app = express()
 
 
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+app.use(morgan("dev"))
 
 app.use('/api/notes', notesRoutes)
 
--- notes-application/frontend/src/pages/Note.jsx
+++ notes-application/frontend/src/pages/Note.jsx
@@ -1,34 +1,90 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import './Note.css'
+import api from '../api/axios.js'
 const Note = () => {
-  return (
-    <div>
-        <div className="container">
-            <div className="top">
-                <h1>Notes app</h1>
-            <h3>Add notes</h3>
 
-            <form>
-                <div className='title'>
-                    <input type="text" placeholder='notes title...'/>
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [notes, setNotes] = useState([])
+
+
+    useEffect(() => {
+        api.get('/notes')
+            .then((response) => setNotes(response.data.notes))
+            .catch((error) => error)
+    }, [])
+
+    function handleChange(e) {
+        setTitle(e.target.value)
+        // setDescription(e.target.value)
+    }
+    function handleDes(e) {
+        setDescription(e.target.value)
+    }
+
+    const createNotes = async (e) => {
+        e.preventDefault()
+
+        if (!title.trim() || !description.trim()) {
+            alert("All field required");
+            return;
+        }
+        try {
+            const response = await api.post('/notes/create', { title, description })
+            setNotes([...notes, response.data.note])
+            setTitle("");
+            setDescription("")
+
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+    return (
+        <>
+            <div>
+                <div className="container">
+                    <div className="top">
+                        <h1>Notes app</h1>
+                        <h3>Add notes</h3>
+
+                        <form>
+                            <div className='title'>
+                                <input type="text"
+                                    placeholder='notes title...'
+                                    value={title}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div className="desc">
+                                <textarea type="text"
+                                    placeholder='Notes description'
+                                    value={description}
+                                    onChange={handleDes}
+                                />
+                            </div>
+                            <button type='submit' onClick={createNotes}>Create note</button>
+                        </form>
+                    </div>
+                    {
+                        notes.map((note) => (
+
+                            <div className="card" key={note._id}>
+                                <h3>{note.title}</h3>
+                                <p>{note.description}</p>
+                                <div className="buttons">
+                                    <button>Edit</button>
+                                    <button>Delete</button>
+                                </div>
+                            </div>
+                        ))
+
+                    }
                 </div>
-               <div className="desc">
-                 <textarea type="text" placeholder='Notes description' />
-               </div>
-                <button>Create note</button>
-            </form>
             </div>
-            <div className="card">
-                <h3>MERN stack</h3>
-                <p>Important topics of read</p>
-                <div className="buttons">
-                    <button>Edit</button>
-                    <button>Delete</button>
-                </div>
-            </div>
-        </div>
-    </div>
-  )
+        </>
+
+    )
 }
 
 export default Note
