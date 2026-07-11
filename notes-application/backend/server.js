d4f7e1916b6aef33ba5858849eef4269356d9b47
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-07-11 22:31:20 +0530

    fix server start function

--- notes-application/backend/server.js
+++ notes-application/backend/server.js
@@ -1,12 +1,14 @@
 import app from "./src/app.js";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import connectDb from "./src/config/db.js";
-dotenv.config()
-const PORT = process.env.PORT 
+dotenv.config();
+const PORT = process.env.PORT;
 
-connectDb();
-
-app.listen(PORT, ()=>{
+const startServer = async () => {
+  await connectDb();
+  app.listen(PORT, () => {
     console.log(`Server is running on port : ${PORT}`);
-})
+  });
+};
 
+startServer();
