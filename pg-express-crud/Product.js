543533b889a75714a71d64603d1ab54a57008e23
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-07-14 16:06:01 +0530

    Init Express API with PostgreSQL and Sequelize CRUD

---
+++ pg-express-crud/Product.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("./database");
+
+const Product = sequelize.define("Product", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  inStock: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+});
+
+module.exports = Product;
